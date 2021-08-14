27ede200bbba62bc401c279be65d920e8b6b216a
日置　貴大 <h.takahiro1330@gmail.com>
2021-08-14 22:39:21 +0900

    fix: footer displayed year

--- src/components/organisms/common/Footer.tsx
+++ src/components/organisms/common/Footer.tsx
@@ -2,6 +2,6 @@ import { FC } from 'react'
 
 export const Footer: FC = () => (
   <footer className="h-16 flex items-center justify-center text-center bg-gray-100 h-footer">
-    © 2020 Hioki Takahiro
+    © {new Date().getFullYear()} Hioki Takahiro
   </footer>
 )
