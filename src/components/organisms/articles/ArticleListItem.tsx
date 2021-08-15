78390fca2b4f7b6427150db92b05b1911d1549cb
日置　貴大 <h.takahiro1330@gmail.com>
2021-08-15 16:08:02 +0900

    add: use next image

--- src/components/organisms/articles/ArticleListItem.tsx
+++ src/components/organisms/articles/ArticleListItem.tsx
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs'
 import { FC } from 'react'
 import { Paper } from '~/components/atoms/Paper'
+import Image from 'next/image'
 
 type Props = {
   article: Article
@@ -9,17 +10,20 @@ type Props = {
 export const ArticleListItem: FC<Props> = ({ article }) => (
   <Paper>
     <section className="h-80 max-w-md mx-auto hover:opacity-80 hover:bg-gray-50">
-      {article.mainVisual ? (
-        <img
-          className="h-2/3 w-full object-cover rounded-t-lg"
-          src={article.mainVisual ? article.mainVisual.url : ''}
-          alt="記事のメインビジュアル"
-        />
-      ) : (
-        <div className="h-2/3 flex items-center justify-center">
-          Camper's Blog
-        </div>
-      )}
+      <div className="relative h-2/3 w-full">
+        {article.mainVisual?.url ? (
+          <Image
+            placeholder="blur"
+            blurDataURL={article.mainVisual.url}
+            layout="fill"
+            className="object-cover rounded-t-lg"
+            src={article.mainVisual.url}
+            alt="記事のメインビジュアル"
+          />
+        ) : (
+          <p className="flex items-center justify-center">Camper's Blog</p>
+        )}
+      </div>
       <div className="border-t h-1/3 p-4 flex flex-col justify-between">
         <h2 className="font-bold multiple-ellipsis">{article.title}</h2>
         <span className="block w-full text-right text-gray-500 text-sm">
